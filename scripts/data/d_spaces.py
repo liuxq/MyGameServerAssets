4f2d1e47e0bc042766a33bd77201faa9dded5ae0
liuxq <lxqiang87@126.com>
2015-12-04 18:05:04 +0800

    12

---
+++ scripts/data/d_spaces.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+datas={1: {'entityType': 'Space', 'spawnPos': (771.5861, 211.0021, 776.5501), 'resPath': 'xinshoucun', 'id': 1, 'type': 1, 'name': '新手村'}, 2: {'entityType': 'Space', 'spawnPos': (-97.9299, 0.0, -158.922), 'resPath': 'kbengine_ogre_demo', 'id': 2, 'type': 1, 'name': 'kbengine_ogre_demo'}, 3: {'entityType': 'Space', 'spawnPos': (-97.9299, 1.5, -158.922), 'resPath': 'kbengine_unity3d_demo', 'id': 3, 'type': 1, 'name': 'kbengine_unity3d_demo'}, 4: {'entityType': 'Space', 'spawnPos': (0.0, 1.5, 0.0), 'resPath': 'teleportspace', 'id': 4, 'type': 1, 'name': 'teleportspace'}, 5: {'entityType': 'SpaceDuplicate', 'spawnPos': (0.0, 0.0, 0.0), 'resPath': 'duplicate', 'id': 5, 'type': 2, 'name': 'Duplicate(副本)'}, 6: {'entityType': 'Space', 'spawnPos': (108.0, 0.0, 90.0), 'resPath': 'kbengine_cocos2d_js_demo', 'id': 6, 'type': 1, 'name': 'kbengine_cocos2d_js_demo'}}
+
+allDatas = {
+	'场景表':datas,
+}
