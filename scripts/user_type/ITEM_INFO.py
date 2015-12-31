902e76cd1f009537de42a7986085213416d12e81
liuxq <lxqiang87@126.com>
2015-12-31 18:01:24 +0800

    12

---
+++ scripts/user_type/ITEM_INFO.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+import KBEngine
+import GlobalConst
+from KBEDebug import * 
+
+class TItemInfo(list):
+	"""
+	"""
+	def __init__(self):
+		"""
+		"""
+		list.__init__(self)
+		
+	def asDict(self):
+		data = {
+			"UUID"			: self[0],
+			"itemId"		: self[1],
+			"itemCount"		: self[2],
+		}
+		
+		return data
+
+	def createFromDict(self, dictData):
+		self.extend([dictData["UUID"], dictData["itemId"], dictData["itemCount"]])
+		return self
+		
+class ITEM_INFO_PICKLER:
+	def __init__(self):
+		pass
+
+	def createObjFromDict(self, dct):
+		return TItemInfo().createFromDict(dct)
+
+	def getDictFromObj(self, obj):
+		return obj.asDict()
+
+	def isSameType(self, obj):
+		return isinstance(obj, TItemInfo)
+
+item_info_inst = ITEM_INFO_PICKLER()
+
+class TItemInfoList(dict):
+	"""
+	"""
+	def __init__(self):
+		"""
+		"""
+		dict.__init__(self)
+		
+	def asDict(self):
+		datas = []
+		dct = {"values" : datas}
+
+		for key, val in self.items():
+			datas.append(val)
+			
+		return dct
+
+	def createFromDict(self, dictData):
+		for data in dictData["values"]:
+			self[data[0]] = data
+		return self
+		
+class AVATAR_INFO_LIST_PICKLER:
+	def __init__(self):
+		pass
+
+	def createObjFromDict(self, dct):
+		return TItemInfoList().createFromDict(dct)
+
+	def getDictFromObj(self, obj):
+		return obj.asDict()
+
+	def isSameType(self, obj):
+		return isinstance(obj, TItemInfoList)
+
+item_info_list_inst = AVATAR_INFO_LIST_PICKLER()
