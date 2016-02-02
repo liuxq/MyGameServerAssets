612ab6a6166be45430fc5f3c853327b9661f6a87
liuxq <lxqiang87@126.com>
2016-02-02 14:18:39 +0800

    12

---
+++ scripts/data/d_items.py
@@ -0,0 +1,1 @@
+datas={1: {'id': 1, 'defence': 10 },2: {'id': 2, 'defence': 20 },3: {'id': 3, 'defence': 50 },4: {'id': 4, 'defence': 80 },5: {'id': 5, 'attack_Max': 10, 'attack_Min': 5 },6: {'id': 6, 'attack_Max': 30, 'attack_Min': 15  },7: {'id': 7, 'defence': 20 }}
