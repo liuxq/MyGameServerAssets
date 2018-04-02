13cc620e4efc8580090609ed3ff1f635f92c9c7e
liuxq <lxqiang87@126.com>
2018-04-02 22:37:29 +0800

    up

--- scripts/data/d_entities.py
+++ scripts/data/d_entities.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 
-datas={40001002: {'id': 40001002, 'name': '传送门', 'runSpeed': 0, 'entityType': 'Gate', 'moveSpeed': 0, 'dialogID': 0, 'modelID': 40001001, 'etype': 1},80008001: {'id': 80008001, 'name': '小怪', 'HP_Max': 100, 'runSpeed': 60, 'entityType': 'Monster', 'moveSpeed': 30, 'attack_Max':10,'attack_Min':0, 'defence':10, 'rating':100, 'dodge':100, 'dialogID': 0, 'modelID': 80008001, 'etype': 1},80008003: {'id': 80008003, 'name': '大怪', 'HP_Max': 200, 'runSpeed': 60, 'entityType': 'Monster', 'moveSpeed': 30, 'attack_Max':150,'attack_Min':50, 'defence':50, 'rating':100, 'dodge':100, 'dialogID': 0, 'modelID': 80008001, 'etype': 1},40001003: {'id': 40001003, 'name': '物品', 'runSpeed': 0, 'entityType': 'DroppedItem', 'moveSpeed': 0, 'dialogID': 0, 'modelID': 40001001, 'etype': 1}, 40001004: {'id': 40001004, 'name': '村长', 'runSpeed': 0, 'entityType': 'NPC', 'moveSpeed': 0, 'dialogID': 1, 'modelID': 40001001, 'etype': 1}}
+datas={
+40001002: {'id': 40001002, 'name': '传送门', 'runSpeed': 0, 'entityType': 'Gate', 'moveSpeed': 0, 'dialogID': 0, 'modelID': 40001001, 'etype': 1},
+80008001: {'id': 80008001, 'name': '小怪', 'HP_Max': 100, 'runSpeed': 60, 'entityType': 'Monster', 'moveSpeed': 30, 'attack_Max':10,'attack_Min':0, 'defence':10, 'rating':100, 'dodge':100, 'dialogID': 0, 'modelID': 80008001, 'etype': 1},
+80008003: {'id': 80008003, 'name': '大怪', 'HP_Max': 200, 'runSpeed': 60, 'entityType': 'Monster', 'moveSpeed': 30, 'attack_Max':150,'attack_Min':50, 'defence':50, 'rating':100, 'dodge':100, 'dialogID': 0, 'modelID': 80008001, 'etype': 1},
+40001003: {'id': 40001003, 'name': '物品', 'runSpeed': 0, 'entityType': 'DroppedItem', 'moveSpeed': 0, 'dialogID': 0, 'modelID': 40001001, 'etype': 1}, 
+40001004: {'id': 40001004, 'name': '村长', 'runSpeed': 0, 'entityType': 'NPC', 'moveSpeed': 0, 'dialogID': 1, 'modelID': 40001001, 'etype': 1},
+80008002: {'id': 80008002, 'name': '稻草人', 'HP_Max': 10000, 'runSpeed': 0, 'entityType': 'Monster', 'moveSpeed': 0, 'attack_Max':0,'attack_Min':0, 'defence':10, 'rating':100, 'dodge':100, 'dialogID': 0, 'modelID': 80008001, 'etype': 1}
+}
 
 allDatas = {
 	'NPC表':datas,
